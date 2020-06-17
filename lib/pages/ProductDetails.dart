26ff98ffb1ff53425f0631df74ece51047406fd9
Navod1996 <44425494+Navod1996@users.noreply.github.com>
2020-06-17 20:25:19 +0530

    upload 1

---
+++ lib/pages/ProductDetails.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+
+class ProductDetails extends StatefulWidget {
+   final productDetails_name;
+  final productDetails_image;
+  final productDetails_oldprise;
+  final productDetails_prise;
+
+  ProductDetails({
+    this.productDetails_image,
+    this.productDetails_name,
+    this.productDetails_oldprise,
+    this.productDetails_prise
+  });
+  @override
+  _ProductDetailsState createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Shop App'),
+      actions: <Widget>[
+       
+        new IconButton(icon: Icon(Icons.shopping_cart,color: Colors.white) , onPressed: null, ),
+         new IconButton(icon: Icon(Icons.search,color: Colors.white) , onPressed: null, )
+      ],
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new Container(
+            margin: new EdgeInsets.symmetric(vertical: 30),   
+            height: 300.0,
+            child: GridTile(
+                
+              
+              child: Container(
+             
+                color: Colors.white70,
+          
+                child: Image.asset(widget.productDetails_image,),
+              ),
+              footer: 
+              new Container(
+                color: Colors.white70,
+                child:ListTile(
+                leading: new Text(widget.productDetails_name,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+                
+                title: new Row(
+                  children: <Widget>[
+                    Expanded(
+                      child:new Text("\$${widget.productDetails_prise}",style: TextStyle(fontWeight: FontWeight.bold))
+                    ),
+                    Expanded(
+                      child: new Text("\$${widget.productDetails_oldprise}",style: TextStyle(fontWeight: FontWeight.bold),
+                    ))
+                  ],
+                ),
+                )
+              ),
+            
+            ),
+            
+            
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(onPressed: (){
+                  showDialog(context: context,
+                  builder: (context){
+                    return new AlertDialog(
+                      title:new Text("Size") ,
+                      content: new Text("Shose the size"),
+                      actions: <Widget>[
+                        new MaterialButton(
+                          onPressed: (){Navigator.of(context).pop(context);},
+                          child: new Text("Close"),
+                        )
+                      ],
+                    );
+                  }
+                  );
+                },
+                color: Colors.white,
+                textColor: Colors.grey,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: new Text("Size",style: TextStyle(fontSize: 16),),
+                    ),
+                    Expanded(
+                        child: new Icon(Icons.arrow_drop_down),
+                    ),
+                    
+                  ],
+                ),
+                
+                ),
+                
+              ),
+               Expanded(
+                child: MaterialButton(onPressed: (){
+                  showDialog(context: context,
+                  builder: (context){
+                    return new AlertDialog(
+                      title:new Text("Size") ,
+                      content: new Text("Shose the size"),
+                      actions: <Widget>[
+                        new MaterialButton(
+                          onPressed: (){Navigator.of(context).pop(context);},
+                          child: new Text("Close"),
+                        )
+                      ],
+                    );
+                  }
+                  );
+                },
+                color: Colors.white,
+                textColor: Colors.grey,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: new Text("Size",style: TextStyle(fontSize: 16),),
+                    ),
+                    Expanded(
+                        child: new Icon(Icons.arrow_drop_down),
+                    ),
+                    
+                  ],
+                ),
+                
+                ),
+                
+              ),
+               Expanded(
+                child: MaterialButton(onPressed: (){ 
+                  showDialog(context: context,
+                  builder: (context){
+                    return new AlertDialog(
+                      title:new Text("Size") ,
+                      content: new Text("Shose the size"),
+                      actions: <Widget>[
+                        new MaterialButton(
+                          onPressed: (){Navigator.of(context).pop(context);},
+                          child: new Text("Close"),
+                        )
+                      ],
+                    );
+                  }
+                  );
+                 },
+                color: Colors.white,
+                textColor: Colors.grey,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: new Text("Size",style: TextStyle(fontSize: 16),),
+                    ),
+                    Expanded(
+                        child: new Icon(Icons.arrow_drop_down),
+                    ),
+                    
+                  ],
+                ),
+                
+                ),
+                
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: new MaterialButton(
+                  onPressed: (){},
+                  color: Colors.red,
+                  elevation: 0.2,
+                  child: Text("Buy now"),
+                ),
+              
+              ),
+              new IconButton(icon: Icon(Icons.shopping_cart),color:Colors.red , onPressed: null,),
+              new IconButton(icon: Icon(Icons.favorite), color:Colors.red,onPressed: null,)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
